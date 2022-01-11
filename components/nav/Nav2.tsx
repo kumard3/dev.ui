f9f8c175e6c5bb8c85a46d264b5b5b4a629d03b6
kumard3 <kumardeepanshu157200@gmail.com>
2022-01-12 00:00:33 +0530

    nav component updated

--- components/nav/Nav2.tsx
+++ components/nav/Nav2.tsx
@@ -1,6 +1,7 @@
 import React, { useRef, useState } from "react";
 import useOnClickOutside from "../useOnClickOutside";
 
+import Link from "next/link";
 
 const navData = [
   {
@@ -11,93 +12,39 @@ const navData = [
     name: "Templates",
     href: "/templates",
   },
-
 ];
 
-
 export default function Nav2() {
+  // Create a ref that we add to the element for which we want to detect outside clicks
   const ref = useRef<HTMLDivElement>(null);
-  
+  const buttonRef = useRef<HTMLDivElement>(null);
+
   // State for our modal
   const [isModalOpen, setModalOpen] = useState(false);
   // Call hook passing in the ref and a function to call on outside click
   useOnClickOutside(ref, () => setModalOpen(false));
+
+
   return (
-    <header className="p-4 dark:bg-gray-800 dark:text-gray-100 w-full">
-      <div className="container flex justify-between h-16 mx-auto">
-        <a
-          href="#"
-          aria-label="Back to homepage"
-          className="flex items-center p-2"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            viewBox="0 0 32 32"
-            className="w-8 h-8 dark:text-indigo-400"
-          >
-            <path d="M27.912 7.289l-10.324-5.961c-0.455-0.268-1.002-0.425-1.588-0.425s-1.133 0.158-1.604 0.433l0.015-0.008-10.324 5.961c-0.955 0.561-1.586 1.582-1.588 2.75v11.922c0.002 1.168 0.635 2.189 1.574 2.742l0.016 0.008 10.322 5.961c0.455 0.267 1.004 0.425 1.59 0.425 0.584 0 1.131-0.158 1.602-0.433l-0.014 0.008 10.322-5.961c0.955-0.561 1.586-1.582 1.588-2.75v-11.922c-0.002-1.168-0.633-2.189-1.573-2.742zM27.383 21.961c0 0.389-0.211 0.73-0.526 0.914l-0.004 0.002-10.324 5.961c-0.152 0.088-0.334 0.142-0.53 0.142s-0.377-0.053-0.535-0.145l0.005 0.002-10.324-5.961c-0.319-0.186-0.529-0.527-0.529-0.916v-11.922c0-0.389 0.211-0.73 0.526-0.914l0.004-0.002 10.324-5.961c0.152-0.090 0.334-0.143 0.53-0.143s0.377 0.053 0.535 0.144l-0.006-0.002 10.324 5.961c0.319 0.185 0.529 0.527 0.529 0.916z"></path>
-            <path d="M22.094 19.451h-0.758c-0.188 0-0.363 0.049-0.515 0.135l0.006-0.004-4.574 2.512-5.282-3.049v-6.082l5.282-3.051 4.576 2.504c0.146 0.082 0.323 0.131 0.508 0.131h0.758c0.293 0 0.529-0.239 0.529-0.531v-0.716c0-0.2-0.11-0.373-0.271-0.463l-0.004-0.002-5.078-2.777c-0.293-0.164-0.645-0.26-1.015-0.26-0.39 0-0.756 0.106-1.070 0.289l0.010-0.006-5.281 3.049c-0.636 0.375-1.056 1.055-1.059 1.834v6.082c0 0.779 0.422 1.461 1.049 1.828l0.009 0.006 5.281 3.049c0.305 0.178 0.67 0.284 1.061 0.284 0.373 0 0.723-0.098 1.027-0.265l-0.012 0.006 5.080-2.787c0.166-0.091 0.276-0.265 0.276-0.465v-0.716c0-0.293-0.238-0.529-0.529-0.529z"></path>
-          </svg>
-        </a>
-        <ul className="items-stretch hidden space-x-3 md:flex">
-          <li className="flex">
-            <a
-              href="#"
-              className="flex items-center px-4 -mb-1  dark:border-transparent"
-            >
-              Link
-            </a>
-          </li>
-          <li className="flex">
-            <a
-              href="#"
-              className="flex items-center px-4 -mb-1  dark:border-transparent"
-            >
-              Link
-            </a>
-          </li>
-          <li className="flex">
-            <a
-              href="#"
-              className="flex items-center px-4 -mb-1  dark:border-transparent dark:text-indigo-400 dark:border-indigo-400"
-            >
-              Link
-            </a>
-          </li>
-          <li className="flex">
-            <a
-              href="#"
-              className="flex items-center px-4 -mb-1  dark:border-transparent"
-            >
-              Link
-            </a>
-          </li>
-        </ul>
-        <button className="flex justify-end p-4 md:hidden">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M4 6h16M4 12h16M4 18h16"
-            ></path>
-          </svg>
-        </button>
-        <div className="-mr-2 -my-2 sm:hidden">
+    // <Popover className= {`sticky top-0 z-40 w-full backdrop-blur flex-none transition-colors duration-500 lg:z-50 lg:border-b lg:border-gray-900/10 bg-white supports-backdrop-blur:bg-white/95 ${show && 'bg-inherit	'}`}>
+    <div className="w-full backdrop-blur-sm  bg-[#818CF8] text-xl font-bold drop-shadow-xl flex-none transition-colors duration-500 lg:z-50 ">
+      <div className="w-full container mx-auto">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 ">
+          <div className="flex justify-between items-center   py-6 sm:justify-between sm:space-x-10">
+            <div className="flex justify-start items-center text-center lg:w-0 lg:flex-1">
+              
+              <Link href="#home">
+                <h1 className="h-[3.5rem] flex items-center text-center" > Dev.ui </h1>
+              </Link>
+            </div>
+            <div className=" sm:hidden">
               <>
                 {isModalOpen ? (
                   <div
                     ref={ref}
-                    className="fixed w-[24rem] right-0 top-0   p-2 h-[100vh]  transition transform md:hidden"
+                    className="fixed w-[24rem] right-0 top-0   h-[100vh]  transition  translate-x-[-1px] md:hidden"
                   >
-                    <div className="rounded-lg shadow-lg  ring-1 h-full ring-black ring-opacity-5 bg-[#141628] border-[1px] border-red-100/20 text-white divide-y-2 divide-gray-50">
+                    <div className="rounded-lg shadow-lg  ring-1 h-full ring-black ring-opacity-5 bg-[#818CF8] border-[1px] border-red-100/20 text-white divide-y-2 divide-gray-50">
                       <div className="pt-5 pb-6 px-5">
                         <div className="-mr-2 float-right mb-3">
                           <button
@@ -166,7 +113,20 @@ export default function Nav2() {
                 )}
               </>
             </div>
+            <nav className="hidden sm:flex space-x-10 items-center">
+              {navData.map((n) => {
+                return (
+                  <Link key={n.name} href={n.href}>
+                    {n.name}
+                  </Link>
+                );
+              })}
+            </nav>
+          </div>
+        </div>
       </div>
-    </header>
+    </div>
   );
 }
+
+

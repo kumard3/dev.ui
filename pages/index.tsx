468ca8b39ad475eb88536ce6c7ad43bf83250cd9
kumard3 <kumardeepanshu157200@gmail.com>
2022-01-16 23:18:26 +0530

    seo added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,15 @@
 /* eslint-disable @next/next/link-passhref */
 import * as React from "react";
-import { Feature1 } from "../components/features/Features1";
 import Hero from "../components/Hero";
-import ReactDOM from "react-dom";
-// import Feature1 from "../components/features/Features1"
-import { codeData } from "../data/components/data";
-import ReactDOMServer from "react-dom/server";
+import Seo from "../components/template/components/Seo";
 
-const Hello = () => <div>hello</div>;
-
-const html = ReactDOMServer.renderToString(codeData[1].store[0].component);
-
-console.log(html.toString());
 
 export default function Home() {
 
-console.log(html)
-const tms =  html.toString()
-console.log(codeData[1].store[0])
-
   return (
     <div className="overflow-hidden">
+      <Seo title={"Devui"} description={"Dev UI is a free, open-source collection of UI components and templates based on Tailwind CSS"} image={""} url={""} />
       <Hero />
-      {/* {tms}
-      <div dangerouslySetInnerHTML={{ __html: tms }} /> */}
     </div>
   );
 }

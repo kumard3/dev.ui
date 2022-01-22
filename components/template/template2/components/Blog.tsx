120495a2364cfe0838d15dd863ae5fae37a6f3e2
kumard3 <kumardeepanshu157200@gmail.com>
2022-01-22 22:46:55 +0530

    clean up

--- components/template/template2/components/Blog.tsx
+++ components/template/template2/components/Blog.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function Blog() {
   return (
     <section className="">
@@ -82,15 +80,7 @@ export default function Blog() {
               </p>
             </div>
           </a>
-          {/* 
-          <a href="#" className="max-w-sm mx-auto group hover:no-underline focus:no-underline  hidden sm:block">
-              <img role="presentation" className="object-cover w-full rounded h-44 " src="https://source.unsplash.com/random/486x366"/>
-              <div className="p-6 space-y-2">
-                  <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">In usu laoreet repudiare legendos</h3>
-                  <span className="text-xs dark:text-coolGray-400">January 26, 2021</span>
-                  <p>Mei ex aliquid eleifend forensibus, quo ad dicta apeirian neglegentur, ex has tantas percipit perfecto. At per tempor albucius perfecto, ei probatus consulatu patrioque mea, ei vocent delicata indoctum pri.</p>
-              </div>
-          </a> */}
+          
         </div>
       
       </div>

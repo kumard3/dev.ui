a0366033b16012ee3c2069ea529faa38390591e8
kumard3 <kumardeepanshu157200@gmail.com>
2022-01-22 22:50:02 +0530

    cleanup

--- components/template/template1/components/TestimonialCard.tsx
+++ components/template/template1/components/TestimonialCard.tsx
@@ -4,12 +4,7 @@ export default function TestimonialCard() {
         <div className="container px-6 py-12 mx-auto">
             <div className="grid items-center gap-4 xl:grid-cols-5">
                 <div className="max-w-2xl mx-auto my-8 space-y-4 text-center xl:col-span-2 xl:text-left">
-                    {/* <h2 className="text-4xl font-bold">Lorem ipsum dolor sit amet consectetur </h2>
-                    <p className="">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus voluptas culpa iure autem, harum nulla maiores ex aspernatur, a reprehenderit rem ab officiis! </p>
-              
-               */}
-               <img src="/Awards 1.png" />
-               
+               <img src="/Awards 1.png" />               
                 </div>
                 <div className="p-6 xl:col-span-3">
                     <div className="grid gap-4 md:grid-cols-2">
--- components/template/template2/LandingPage2.tsx
+++ components/template/template2/LandingPage2.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @next/next/link-passhref */
-import Link from "next/link";
+
 import * as React from "react";
 import Hero from "./components/Hero";
 import NavComponent from "./components/NavComponent";

cc3dcd28f7b896ecc9f654ef6db8cbd9c3adf192
kumard3 <kumardeepanshu157200@gmail.com>
2022-01-22 22:48:28 +0530

    clean up

--- components/template/template2/components/Hero.tsx
+++ components/template/template2/components/Hero.tsx
@@ -1,8 +1,6 @@
 export default function Hero() {
   return (
     <div className="container w-full mx-auto flex flex-wrap justify-between mt-[2rem]">
-    
-
       <div className="flex flex-col items-start justify-center max-w-xl  px-[30px]">
         <h1 className="text-7xl font-bold  text-left">Let's get fusely together</h1>
         <p className="text-md my-5">

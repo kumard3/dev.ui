8b2c959f071b63c95a83e3a2afde20d9f4e9cc32
kumard3 <kumardeepanshu157200@gmail.com>
2022-02-22 02:28:36 +0530

    style updated

--- website/components/ComponentButton.tsx
+++ website/components/ComponentButton.tsx
@@ -9,7 +9,7 @@ export const Componentbutton = ({ title, url, data }: any) => {
     <Link href={`${url}${title}`} passHref>
       <button
         className={`${
-          router.asPath == `${url}${title}` ? "bg-gray-600" : ""
+          router.asPath == `${url}${title}` ? "bg-gray-600" : " border-2 border-gray-600"
         } py-2 px-4 shadow  text-left active: text-lg w-full rounded `}
       >
         {title}

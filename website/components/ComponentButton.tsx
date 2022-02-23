062569aa9747e1a36e57e56c2ada0116421d4149
kumard3 <kumardeepanshu157200@gmail.com>
2022-02-23 12:44:39 +0530

    style bug fixed

--- website/components/ComponentButton.tsx
+++ website/components/ComponentButton.tsx
@@ -9,12 +9,20 @@ export const Componentbutton = ({ title, url, data }: any) => {
     <Link href={`${url}${title}`} passHref>
       <button
         className={`${
-          router.asPath == `${url}${title}` ? "bg-white/10 backdrop-blur-sm" : " text-[#999999] "
+          router.asPath == `${url}${title}`
+            ? "bg-white/10 backdrop-blur-sm"
+            : " text-[#999999] "
         } 
      
  py-2 px-4 shadow  text-left active: text-lg w-full rounded flex hover:bg-white/10 hover:backdrop-blur-sm items-center`}
       >
-        <span className="min-w-[4px] min-h-[4px] block rounded-[50%] mr-3 bg-slate-500"></span>
+        <span
+          className={`${
+            router.asPath == `${url}${title}`
+              ? "bg-white"
+              : "bg-slate-500"
+          } min-w-[4px] min-h-[4px] block rounded-[50%] mr-3 `}
+        ></span>
         {title}
       </button>
     </Link>

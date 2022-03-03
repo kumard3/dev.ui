62b009b270f29d7e02c349f8bef4b1cbcd5aa46a
kumard3 <kumardeepanshu157200@gmail.com>
2022-03-03 18:03:22 +0530

    new template added

--- data/templates/data.tsx
+++ data/templates/data.tsx
@@ -8,6 +8,12 @@ export const showCode = [
         Weburl: "https://zealous-curran-3560b0.netlify.app/",
         github:
           "https://github.com/kumard3/dev-ui-templates/tree/main/template-2",
+      },     
+       {
+        title: "Portfolio",
+        Weburl: "https://boring-lamarr-42ae45.netlify.app/",
+        github:
+          "https://github.com/kumard3/dev-ui-templates/tree/main/template-3",
       },
     ],
   },

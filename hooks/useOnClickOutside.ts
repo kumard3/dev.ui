ebca2032d384b445a845f25fa281fbd427ed279e
Kumar Deepanshu <62591080+kumard3@users.noreply.github.com>
2022-03-20 09:29:23 +0530

    Update useOnClickOutside.ts

--- hooks/useOnClickOutside.ts
+++ hooks/useOnClickOutside.ts
@@ -22,12 +22,7 @@ const useOnClickOutside = (ref:any, handler:any ) => {
         document.removeEventListener('touchstart', listener);
       };
     },
-    // Add ref and handler to effect dependencies
-    // It's worth noting that because passed in handler is a new ...
-    // ... function on every render that will cause this effect ...
-    // ... callback/cleanup to run every render. It's not a big deal ...
-    // ... but to optimize you can wrap handler in useCallback before ...
-    // ... passing it into this hook.
+
     [ref, handler],
   );
 };

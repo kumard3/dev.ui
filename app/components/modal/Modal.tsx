98be303b031b54228d0cea5957547858e93a1901
kumard3 <kumardeepanshu157200@gmail.com>
2022-03-26 07:59:46 +0530

    modal bug solved

--- app/components/modal/Modal.tsx
+++ app/components/modal/Modal.tsx
@@ -1,20 +1,17 @@
-import { useState } from "react";
+import { useState } from 'react'
 
 export default function Modal() {
-  const [isModalOpen, setModalOpen] = useState(false);
+  const [isModalOpen, setModalOpen] = useState(false)
 
   return (
     <div className=" flex flex-col">
-      <button
-        className="bg-white/20 p-3 rounded-xl"
-        onClick={() => setModalOpen(true)}
-      >
+      <button className="bg-white/20 p-3 rounded-xl" onClick={() => setModalOpen(true)}>
         click me
       </button>
       <>
         {isModalOpen ? (
           <div
-            className="fixed z-10 inset-0 overflow-y-auto"
+            className="fixed z-50 inset-0 overflow-y-auto"
             aria-labelledby="modal-title"
             role="model"
             aria-modal="true"
@@ -35,21 +32,16 @@ export default function Modal() {
               <div className="inline-block align-bottom bg-white dark:bg-black/80 dark:text-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
                 <div className="bg-white dark:bg-black/80 dark:text-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                   <div className="sm:flex sm:items-start">
-                    
                     <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                      <h3
-                        className="text-lg leading-6 font-medium text-gray-900 "
-                        id="modal-title"
-                      >
+                      <h3 className="text-lg leading-6 font-medium text-gray-900 " id="modal-title">
                         Lorem ipsum
                       </h3>
                       <div className="mt-2">
                         <p className="text-sm text-gray-500 dark:bg-black/80 dark:text-white">
-                          dolor sit amet consectetur adipisicing elit. Corporis
-                          atque beatae exercitationem. Eligendi sit excepturi
-                          harum necessitatibus. Veritatis sequi nemo corporis,
-                          autem, itaque quae debitis error laboriosam recusandae
-                          optio ea!
+                          dolor sit amet consectetur adipisicing elit. Corporis atque beatae
+                          exercitationem. Eligendi sit excepturi harum necessitatibus. Veritatis
+                          sequi nemo corporis, autem, itaque quae debitis error laboriosam
+                          recusandae optio ea!
                         </p>
                       </div>
                     </div>
@@ -75,9 +67,9 @@ export default function Modal() {
             </div>
           </div>
         ) : (
-          ""
+          ''
         )}
       </>
     </div>
-  );
+  )
 }
--- website/codeDisplay/CodeDisplay.tsx
+++ website/codeDisplay/CodeDisplay.tsx
@@ -1,6 +1,141 @@
+// /* eslint-disable @next/next/no-sync-scripts */
+
+// import { useState, ReactNode, useEffect } from 'react'
+
+// import { Prism } from '@mantine/prism'
+
+// interface Props {
+//   component?: ReactNode
+//   jsx?: ReactNode
+// }
+
+// export default function CodeDisplay({ component, jsx }: Props) {
+//   const [opened, setOpened] = useState('100%')
+//   const [tab, setTab] = useState('Preview')
+//   const data = [
+//     {
+//       title: 'Preview',
+//       data: component,
+//       code: '',
+//     },
+//     {
+//       title: 'Jsx',
+//       data: '',
+//       code: jsx,
+//     },
+//   ]
+
+//   const widths = [
+//     {
+//       width: '640px',
+//       title: 'Mobile',
+//     },
+//     {
+//       width: '768px',
+//       title: 'Small',
+//     },
+//     {
+//       width: '1024px',
+//       title: 'Medium',
+//     },
+//     {
+//       width: '1280px',
+//       title: 'Large',
+//     },
+//     {
+//       width: '100%',
+//       title: 'Full',
+//     },
+//   ]
+
+//   const filterdata = data.filter((e) => e.title === tab)
+
+//   return (
+//     <>
+//       <section className=" mb-5 min-h-[10rem] w-full flex flex-col  container mx-auto overflow-hidden rounded-lg shadow-sm scrollbar-none my-5 ">
+//         <section className="flex bg-[#111111] p-3 justify-center items-center lg:justify-between">
+//           <div>
+//             {data.map((n, index) => {
+//               return (
+//                 <>
+//                   <button onClick={() => setTab(n.title)} className="p-3 " key={index}>
+//                     {n.title}
+//                   </button>
+//                 </>
+//               )
+//             })}
+//           </div>
+//           <div className="lg:flex justify-between hidden ">
+//             {widths.map((n, index) => {
+//               return (
+//                 <>
+//                   <button
+//                     key={index}
+//                     onClick={() => setOpened(n.width)}
+//                     className="px-5 py-2  mx-1 rounded-xl border "
+//                   >
+//                     {n.title}
+//                   </button>
+//                 </>
+//               )
+//             })}
+//           </div>
+//         </section>
+//         <hr />
+
+//         <div>
+//           <style jsx>{`
+//             .widthTest {
+//               width: ${opened};
+//             }
+//             @media (max-width: 1025px) {
+//               .widthTest {
+//                 width: 100%;
+//               }
+//             }
+//           `}</style>
+//           <>
+//             {filterdata.map((n) => {
+//               return (
+//                 <>
+//                   {n.data === '' ? (
+//                     ''
+//                   ) : (
+//                     <div className="max-h-[50rem] overflow-scroll bg-[#151515] backdrop-blur-xl widthTest">
+//                       <section className=" p-3 flex flex-col items-center justify-center w-full ">
+//                         {n.data}
+//                       </section>
+//                     </div>
+//                   )}
+
+//                   {n.code === '' ? (
+//                     ''
+//                   ) : (
+//                     <div className="max-h-[50rem] overflow-scroll">
+//                       <Prism
+//                         colorScheme="dark"
+//                         language="jsx"
+//                         copyLabel="Copy code to clipboard"
+//                         copiedLabel="Code copied to clipboard"
+//                         withLineNumbers={true}
+//                       >
+//                         {n.code}
+//                       </Prism>
+//                     </div>
+//                   )}
+//                 </>
+//               )
+//             })}
+//           </>
+//         </div>
+//       </section>
+//     </>
+//   )
+// }
+
 /* eslint-disable @next/next/no-sync-scripts */
 
-import { useState, ReactNode, useEffect } from 'react'
+import { useState, ReactNode } from 'react'
 
 import { Prism } from '@mantine/prism'
 
@@ -27,19 +162,19 @@ export default function CodeDisplay({ component, jsx }: Props) {
 
   const widths = [
     {
-      width: '640px',
+      width: '340px',
       title: 'Mobile',
     },
     {
-      width: '768px',
+      width: '640px',
       title: 'Small',
     },
     {
-      width: '1024px',
+      width: '768px',
       title: 'Medium',
     },
     {
-      width: '1280px',
+      width: '1024px',
       title: 'Large',
     },
     {
@@ -52,8 +187,8 @@ export default function CodeDisplay({ component, jsx }: Props) {
 
   return (
     <>
-      <section className=" mb-5 min-h-[10rem] w-full flex flex-col  container mx-auto overflow-hidden rounded-lg shadow-sm scrollbar-none my-5 ">
-        <section className="flex bg-[#111111] p-3 justify-center items-center lg:justify-between">
+      <section className="bg-[#120e0e] mb-5 min-h-[10rem] w-full flex flex-col  container mx-auto overflow-hidden rounded-lg shadow-sm scrollbar-none my-5 ">
+        <section className="flex p-3 bg-[#111111] justify-center items-center lg:justify-between">
           <div>
             {data.map((n, index) => {
               return (
@@ -101,7 +236,7 @@ export default function CodeDisplay({ component, jsx }: Props) {
                   {n.data === '' ? (
                     ''
                   ) : (
-                    <div className="max-h-[50rem] overflow-scroll bg-[#151515] backdrop-blur-xl widthTest">
+                    <div className={` max-h-[50rem] overflow-scroll bg-[#181818] widthTest`}>
                       <section className=" p-3 flex flex-col items-center justify-center w-full ">
                         {n.data}
                       </section>
@@ -111,7 +246,7 @@ export default function CodeDisplay({ component, jsx }: Props) {
                   {n.code === '' ? (
                     ''
                   ) : (
-                    <div className="max-h-[50rem] overflow-scroll">
+                    <div className={`max-h-[20rem] overflow-scroll `}>
                       <Prism
                         colorScheme="dark"
                         language="jsx"

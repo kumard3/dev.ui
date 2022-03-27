6fe469e55feecb1d097da5477f1e3514754c443c
kumard3 <kumardeepanshu157200@gmail.com>
2022-03-27 22:35:40 +0530

    new button

---
+++ app/components/button/Button4.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Button4() {
+  return (
+    <button className="relative rounded font-bold bg-[#6c757d] px-10 py-2 shadow-[0_5px_#495057] duration-300 ease-in-out hover:top-[1px] hover:shadow-[0_3px_#495057] active:shadow-[#495057]">
+      Button
+    </button>
+  )
+}

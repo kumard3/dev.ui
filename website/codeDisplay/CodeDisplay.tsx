f4fcfa660f369e1e9424abddb28d99e40d37f3f6
kumard3 <kumardeepanshu157200@gmail.com>
2022-04-16 21:52:54 +0530

    sero updated

--- website/codeDisplay/CodeDisplay.tsx
+++ website/codeDisplay/CodeDisplay.tsx
@@ -49,27 +49,23 @@ export default function CodeDisplay({ component, jsx }: Props) {
     <>
       <Head>
         <meta name="twitter:card" content="summary_large_image" />
-        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
         <meta
           name="description"
           content="Dev UI is a free, open-source collection of UI components and templates based on Tailwind CSS"
         />
         <meta property="og:locale" content="en_US" />
         <meta property="og:type" content="website" />
-        <meta property="og:title" content={`Dev.ui|${title}`} />
+        <meta property="og:title" content="Dev.ui|component" />
         <meta
           property="og:description"
           content="Dev UI is a free, open-source collection of UI components and templates based on Tailwind CSS"
         />
         <meta property="og:url" content="https://dev-ui.vercel.app/" />
-        <meta property="og:site_name" content={`Dev.ui|${title}`} />
+        <meta property="og:site_name" content="Dev.ui|component" />
         <meta property="og:image" content="https://dev-ui.vercel.app/preview.png" />
         <meta property="og:image:secure_url" content="https://dev-ui.vercel.app/" />
         <meta property="og:image:width" content="1440" />
         <meta property="og:image:height" content="800" />
-        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
       </Head>
       <section className="bg-[#120e0e] mb-5 min-h-[10rem] w-full flex flex-col  container mx-auto overflow-hidden rounded-lg shadow-sm scrollbar-none my-5 ">
         <section className="flex p-3 bg-[#111111] justify-center items-center lg:justify-between">
--- website/codeDisplay/TemplateDisplay.tsx
+++ website/codeDisplay/TemplateDisplay.tsx
@@ -22,29 +22,25 @@ export default function TemplateDisplay({ Weburl, github, images, title }: Props
 
   return (
     <>
-      <Head>
+    <Head>
         <meta name="twitter:card" content="summary_large_image" />
-        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
         <meta
           name="description"
           content="Dev UI is a free, open-source collection of UI components and templates based on Tailwind CSS"
         />
         <meta property="og:locale" content="en_US" />
         <meta property="og:type" content="website" />
-        <meta property="og:title" content={`Dev.ui|${title}`} />
+        <meta property="og:title" content="Dev.ui|component" />
         <meta
           property="og:description"
           content="Dev UI is a free, open-source collection of UI components and templates based on Tailwind CSS"
         />
         <meta property="og:url" content="https://dev-ui.vercel.app/" />
-        <meta property="og:site_name" content={`Dev.ui|${title}`} />
+        <meta property="og:site_name" content="Dev.ui|component" />
         <meta property="og:image" content="https://dev-ui.vercel.app/preview.png" />
         <meta property="og:image:secure_url" content="https://dev-ui.vercel.app/" />
         <meta property="og:image:width" content="1440" />
         <meta property="og:image:height" content="800" />
-        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
       </Head>
 
       <section className="container bg-[#111111] mx-auto max-w-lg 2xl:max-w-xl overflow-hidden  scrollbar-none my-5 ">

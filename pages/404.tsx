16b5f691c6f0707f4a1be7343310aefd1a1cb52b
kumard3 <kumardeepanshu157200@gmail.com>
2022-05-13 23:20:41 +0530

    updated

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,14 +1,24 @@
 import Head from 'next/head'
+import Nav from '../website/components/Nav'
 
 export default function Error() {
   return (
-    <div className="h-screen overflow-hidden">
+    <>
       <Head>
         <title>Dev-ui</title>
       </Head>
-      <div className="w-full flex justify-center ">
-        <h1>404</h1>
+      <div className="w-full ">
+        <Nav />
+        <div className="w-full flex justify-center py-10 ">
+          <img
+            src="https://res.cloudinary.com/ddcg0rzlo/image/upload/v1652463672/giphy_awwf25.gif"
+            alt="404"
+            width="400"
+            height="400"
+            className="rounded-xl"
+          />
+        </div>
       </div>
-    </div>
+    </>
   )
 }
